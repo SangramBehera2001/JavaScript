9974e97bae5653fedaa45541c06d633ffce6ee69
Sangram Behera <sangrambehera2001@gmail.com>
2025-04-02 19:21:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//alert("hello") -> Error, as we are using nodejs, not browser
+
+let name = "gudu"
+let age = 24
+let isLoggedIn = false
+let state;
+
+//number
+//bigint
+//string
+//booleand
+//null -> standalone value
+//undefined -> value is not assigned
+//symbol -> unique
+
+//object
+
+console.log(typeof "gudu");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
