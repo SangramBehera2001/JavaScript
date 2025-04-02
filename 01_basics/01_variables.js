cc8775faf26fb4209ddbcbda28f818c2c9e63a4c
Sangram Behera <sangrambehera2001@gmail.com>
2025-04-02 19:21:35 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sangram@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
